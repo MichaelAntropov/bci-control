0b12d19c56a2fa5a2a0fe843f989b59f5fb34647
Michael Antropov <mikhael.antropov@gmail.com>
2023-05-26 17:53:26 +0300

    Implement command threshold

--- simple-control/bci_controls_test.py
+++ simple-control/bci_controls_test.py
@@ -1,4 +1,5 @@
 from pylsl import StreamInlet, resolve_stream
+from time import sleep
 
 # Gets data from BCI GUI LSL stream and outputs directions
 # Best to use 8 channels when Focus Widget is used
@@ -9,6 +10,10 @@ streams_focus = resolve_stream('name', 'obci_eeg2')
 # create a new inlet to read from the stream
 inlet_emg = StreamInlet(streams_emg[0])
 inlet_focus = StreamInlet(streams_focus[0])
+
+command_sample_threshold = 20
+focus_sample_counter = 0
+
 
 while True:
     sample_emg, _ = inlet_emg.pull_sample()
@@ -21,4 +26,11 @@ while True:
     elif sample_emg[1] > 0.8:  # Blink several times
         print("BACK")
     elif sample_focus[0] > 0.8: # Concentrate
-        print("FORWARD")
+        if focus_sample_counter > 0:
+            if focus_sample_counter > command_sample_threshold:
+                focus_sample_counter = 0
+            else:
+                focus_sample_counter += 1
+        else:
+            print("FORWARD")
+            focus_sample_counter += 1
