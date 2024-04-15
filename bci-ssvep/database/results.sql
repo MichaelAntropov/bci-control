e75f28268bd93f3ec4950663204cfd96296fdcdd
Yehor Katkov <43516762+yegorkt25@users.noreply.github.com>
2024-04-15 10:54:21 -0700

    Added experiment recorder files (#11)
    * Added experiment recorder files
    
    * Moved experiment recorder files from src to root folder

--- bci-ssvep/database/results.sql
+++ bci-ssvep/database/results.sql
@@ -2,4 +2,20 @@ select cast(sum(json_extract(process_results.results, '$.total_correct_itcca')) 
        cast(sum(json_extract(process_results.results, '$.total_correct_mix')) as double)/cast(sum(json_extract(process_results.results, '$.total_trials_tested')) as double) as mix_accuracy,
        cast(sum(json_extract(process_results.results, '$.total_correct')) as double)/cast(sum(json_extract(process_results.results, '$.total_trials_tested')) as double) as cca_accuracy
 from process_results
-where parameters_id = :processs_param_id
+where parameters_id = :processs_param_id;
+
+
+
+
+
+select cast(sum(json_extract(process_results.results, '$.total_correct_itcca')) as double)/cast(sum(json_extract(process_results.results, '$.total_trials_tested')) as double) as itcca_accuracy,
+       cast(sum(json_extract(process_results.results, '$.total_correct_mix')) as double)/cast(sum(json_extract(process_results.results, '$.total_trials_tested')) as double) as mix_accuracy,
+       cast(sum(json_extract(process_results.results, '$.total_correct')) as double)/cast(sum(json_extract(process_results.results, '$.total_trials_tested')) as double) as cca_accuracy
+from process_results
+join recordings
+    on process_results.recording_id = recordings.id
+where
+    parameters_id = :processs_param_id and subject_id in :subject_id;
+
+
+select * from process_results where json_extract(results, '$.accuracy') = 1
